ed09d9e1905a2abb2deb61a7530497ae5d5a2ba0
Doug von Kohorn <dougvk@gmail.com>
2017-01-24 15:49:22 -0500

    Working, pre-lint.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,20 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+let ConvertLib = artifacts.require("./ConvertLib.sol")
+let MetaCoin = artifacts.require("./MetaCoin.sol")
+let ENS = artifacts.require("./ENS.sol")
+let Registrar = artifacts.require("./Registrar.sol")
+let Deed = artifacts.require("./Deed.sol")
+let PublicResolver = artifacts.require("./PublicResolver.sol")
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-};
+module.exports = (deployer) => {
+  deployer.deploy(ConvertLib)
+  deployer.link(ConvertLib, MetaCoin)
+  deployer.deploy(MetaCoin)
+
+  // The hex string reolves to the TLD 'eth'
+  deployer.deploy(ENS, '0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae')
+    .then(() => {
+      deployer.deploy(Registrar, ENS.deployed().address, '0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae')
+    })
+  deployer.deploy(Deed)
+  deployer.deploy(PublicResolver)
+}
---
+++ test/ENS.js
@@ -0,0 +1,72 @@
+import {default as ENSAuctionLib} from '../lib/ens_registrar'
+let ENS = artifacts.require("./ENS.sol")
+let Registrar = artifacts.require("./Registrar.sol")
+let Deed = artifacts.require("./Deed.sol")
+let PublicResolver = artifacts.require("./PublicResolver.sol")
+let Web3 = require('web3')
+
+contract("ENS integration", (accounts) => {
+  let registrar
+  let auctionRegistrar
+
+  before("set up auction registrar", () => {
+    registrar = Registrar.deployed()
+    auctionRegistrar = new ENSAuctionLib(
+        new Web3.providers.HttpProvider('http://testrpc:8545'),
+        registrar.address,
+        accounts[0]
+    )
+  })
+
+  it("demonstrates that the domain name isn't available", (done) => {
+    auctionRegistrar.available('test')
+      .then((isAvailable) => {
+        assert.isTrue(isAvailable)
+        done()
+      })
+  })
+
+  it("demonstrates that the domain name isn't up for auction", (done) => {
+    auctionRegistrar.upForAuction('test')
+      .then((isUpForAuction) => {
+        assert.isFalse(isUpForAuction)
+        done()
+      })
+  })
+
+  it("can start an auction", (done) => {
+    auctionRegistrar.startAuction('test')
+      .then((started) => {
+        assert.isTrue(started)
+        done()
+      })
+  })
+
+  it("can start a bid", (done) => {
+    auctionRegistrar.createBid('test', accounts[0], '1.123', web3.sha3('secret'))
+      .then(() => registrar.entries(web3.sha3('test')))
+      .then((entry) => {
+        assert.isAbove(entry[2].toNumber() * 1000, Date.now(),
+                       'the end date of the bid is greater than now')
+        done()
+      })
+  })
+
+  it("can reveal a bid", function(done) {
+    auctionRegistrar.createBid('test', accounts[0], '.123', web3.sha3('secret'))
+      .then(function(bidCreated) {
+        assert.isTrue(bidCreated)
+        return registrar.entries(web3.sha3('test'))
+      })
+      .then((entry) => Promise.resolve(assert.isAbove(entry[2].toNumber() * 1000,
+                                       Date.now(),
+                                       'the end date of the bid is greater than now')))
+      .then(() => auctionRegistrar.revealBid('test', accounts[0], '.123', web3.sha3('secret')))
+      .then(() => auctionRegistrar.currentWinner('test'))
+      .then((owner) => {
+        assert.equal(owner, accounts[0], 'owner of the winning bid is correct')
+        done()
+      })
+      .catch(done)
+  })
+})
