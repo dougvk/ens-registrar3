0946249543c254058cd4b1b92d871a34fd635c05
Doug von Kohorn <dougvk@gmail.com>
2017-01-27 10:03:05 -0500

    Get working on travis.

--- truffle.js
+++ truffle.js
@@ -6,6 +6,16 @@ module.exports = {
       host: 'testrpc',
       port: 8545,
       network_id: '*' // Match any network id
+    },
+    ropsten: {
+      host: '159.203.82.211',
+      network_id: '*',
+      port: 7002
+    },
+    staging: {
+      host: '159.203.82.211',
+      network_id: '*',
+      port: 7003
     }
   }
 }
